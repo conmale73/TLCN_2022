8cd4c1391f41af47f8e44d5951f89f9a5aef26ec
duchau <duchauvu0703@gmail.com>
2022-12-23 14:47:38 +0700

    deploy client

--- Admin/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ProductService } from './product.service'
-export { default as AuthService } from './auth.service'
-export { default as DesignerService } from './designer.service'
--- Admin/src/utils/index.js
+++
@@ -1,1 +0,0 @@
-export { default as numberWithCommas } from './numberWithCommas';
